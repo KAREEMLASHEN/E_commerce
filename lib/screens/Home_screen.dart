a4163190aca4766e5848aa73d9c8cb91caa8893e
Islam Abdelwahab <islamabdelwahab99@gmail.com>
2026-04-06 01:57:26 +0200

    convert true to false onboarding

--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_app/screens/login.dart';
+//import 'package:ecommerce_app/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
